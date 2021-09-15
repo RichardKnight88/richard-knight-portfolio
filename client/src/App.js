9e4483ebbf5d6d2a3204361670f0bcb617def81d
richardknight <rknightdesign@gmail.com>
2021-09-15 20:46:50 +0100

    content added and styled

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,290 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
-import axios from 'axios'
+import devicon from 'devicon'
+import rk from './assets/rk.svg'
+import { Container, Row, Col } from 'react-bootstrap'
+import headshot from './assets/headshot.jpg'
+import { sectionHeadings, technologies, otherTechnologies, projects, experience } from './helpers/infoArrays'
 
-function App() {
-  React.useEffect(() => {
-    const getData = async () => {
-      const res = await axios.get('/api/endpoint') // * <-- replace with your endpoint
-      console.log(res.data)
-    }
-    getData()
-  })
+const App = () => {
 
-  return <h1>Hello World</h1>
+  const displayName = (event) => {
+    const assignValue = event.target.outerHTML.toString().replace('<div value="', '').split('"')[0]
+    event.target.innerHTML = `${assignValue}`
+  }
+
+  const hideName = (event) => {
+    event.target.innerHTML = ''
+  }
+
+  const invert = (index) => {
+    if (index % 2 === 0) return 'inverted'
+    if (index % 2 !== 0) return ''
+  }
+
+
+  return (
+    <>
+      <Container
+        fluid
+        className="header"
+      >
+        <div className="logo-name">
+          <img
+            className="logo"
+            src={rk}
+          />
+          <div className="name">
+            <h1>RICHARD</h1>
+            <h1> <span className="bold">KNIGHT</span></h1>
+          </div>
+        </div>
+
+        <div className="nav-links-container">
+          {sectionHeadings.map(item => {
+            const navLink = `#${item.toLocaleLowerCase()}`
+            return (
+              <>
+                <a
+                  key={item}
+                  href={navLink}>
+                  {item.toLocaleUpperCase()}
+                </a>
+                <p>||</p>
+              </>
+            )
+          })}
+        </div>
+
+      </Container>
+
+
+      <section
+        id="about"
+        className="about"
+      >
+
+        <Container className='project'>
+          <Row>
+            <Col
+              xs={12} sm={6}
+              className="bio"
+            >
+              <h3>[ about ]</h3>
+              <h4>
+                I'm a product design engineer turned software developer. I have an inquisitive nature and a passion for aesthetics, however, the most transformative product design ideas are no longer physical. After four years in industry, I pivoted into Software Engineering. I am always trying to learn new things and find great satisfaction in creating and problem solving.
+                <br></br>
+                <br></br>
+                I am always interested in trying new hobbies but mainstays are football (soccer), hot yoga, music and reading. My love of music spans multiple genres and I have the 'Liked Songs' playlist on Spotify to prove it. I am always keen to hear new recommendations! As a former professional drummer I have played all over the UK in various bands - there's nothing quite like live music.
+              </h4>
+            </Col>
+            <Col
+              xs={12} sm={6}
+              className="headshot"
+            >
+              <img
+                src={headshot}
+                alt="Richard Knight Headshot"
+                className="headshot-image"
+              />
+            </Col>
+          </Row>
+        </Container>
+
+      </section>
+
+
+      <section
+        id="technologies"
+        className="skills"
+      >
+
+        <div className="tech-skills">
+          <h3>[ technologies ]</h3>
+          <div className="skills-container">
+            {technologies.map(item => {
+              item = item.replace('.', '')
+              let iconClass = `devicon-${item.toLocaleLowerCase()}-plain`
+              if (item === 'Express' || item === 'Github') {
+                iconClass = `devicon-${item.toLocaleLowerCase()}-original`
+              }
+              if (item === 'npm') {
+                iconClass = `devicon-${item.toLocaleLowerCase()}-original-wordmark`
+              }
+              return (
+                <>
+                  <div
+                    className="skills-icons"
+                    key={item}
+                  >
+                    <i
+                      className={iconClass}
+                      alt={item}
+                    >
+                    </i>
+                    <div
+                      value={item}
+                      className="toggleName"
+                      onMouseEnter={displayName}
+                      onMouseLeave={hideName}
+                    >
+                    </div>
+                  </div>
+                </>
+              )
+            })}
+          </div>
+        </div>
+
+        <div className="other-skills">
+          <h3>[ other ]</h3>
+          <div className="skills-container">
+            {otherTechnologies.map(item => {
+              return (
+                <>
+                  <div
+                    className="skills-icons"
+                    key={item}
+                  >
+                    <i
+                      key={item}
+                      className={`devicon-${item.toLocaleLowerCase()}-plain`}
+                      alt={item}
+                      value={item}
+                    >
+                    </i>
+                    <div
+                      value={item}
+                      className="toggleName"
+                      onMouseEnter={displayName}
+                      onMouseLeave={hideName}
+                    >
+                    </div>
+                  </div>
+                </>
+              )
+            })}
+          </div>
+        </div>
+      </section>
+
+      <section
+        id="projects" 
+        className="projects"
+      >
+
+        <h3>[ projects ]</h3>
+        {projects.map((item, index) => {
+          return (
+            <div
+              key={item.key}
+              className={`project ${invert(index)}`}
+            >
+              <Col xs={12} md={9}>
+                <div className="heading-description-container">
+                  <h2>{item.name.toLocaleUpperCase()}</h2>
+                  <h4 className="description">{item.description}</h4>
+                  <div className="project-links">
+                    <a
+                      href={item.repo}
+                      target="_blank"
+                      rel='noreferrer'
+                    >
+                      <div className="project-icon">
+                        <i
+                          className="devicon-github-original"
+                          alt={`${item.name} github repository`}
+                        >
+                        </i>
+                        <h5>REPO</h5>
+                      </div>
+                    </a>
+                    <a
+                      href={item.deployed}
+                      target="_blank"
+                      rel='noreferrer'
+                    >
+                      <div className="project-icon">
+                        <i className="devicon-chrome-plain"></i>
+                        <h5>APP</h5>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              </Col>
+
+              <Col
+                xs={12} md={3}
+                className="project-image-container"
+              >
+                <div className="project-image-div">
+                  <a
+                    href={item.deployed}
+                    target="_blank"
+                    rel='noreferrer'
+                  >
+                    <img
+                      src={item.image} 
+                      className="project-image" 
+                    />
+                  </a>
+                </div>
+              </Col>
+            </div>
+          )
+        })}
+      </section>
+
+      <section
+        id="experience"
+        className="experience">
+
+        <h3>[ experience ]</h3>
+        <Container>
+          {experience.map((item, index) => {
+            return (
+              <Row
+                key={item.key}
+                className={`project ${invert(index)}`}
+              >
+                <Col 
+                  xs={12} sm={9}
+                  className="heading-description-container exp"
+                >
+                  <h2>{item.company.toLocaleUpperCase()} | {item.title}</h2>
+                  <h4>{item.date}</h4>
+                  {item.description.map((bullet, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className="description"
+                      >
+                        {bullet}
+                      </li>
+                    )
+                  })
+                  }
+                </Col>
+
+                <Col
+                  xs={12} sm={3}
+                  className="project-image-div"
+                >
+                  <img
+                    src={item.image}
+                    className="project-image exp"
+                  />
+                </Col>
+              </Row>
+            )
+          })}
+        </Container>
+
+      </section>
+
+    </>
+  )
 }
 
 export default App
---
+++ client/src/helpers/infoArrays.js
@@ -0,0 +1,108 @@
+import bee from '../assets/projects/bee.png'
+import weather from '../assets/projects/weather2.gif'
+import gods from '../assets/projects/gods.png'
+import buhjit from '../assets/projects/buhjit.png'
+import ga from '../assets/companies/general-assembly-logo.png'
+import tfl from '../assets/companies/tfl-logo.png'
+import eclipse from '../assets/companies/eclipse-logo.png'
+import step from '../assets/companies/step-logo.png'
+import strathy from '../assets/companies/strathy-logo.png'
+
+export const sectionHeadings = ['About', 'Technologies', 'Projects', 'Experience']
+
+export const technologies = ['JavaScript', 'HTML5', 'CSS3', 'React', 'Node.js', 'MongoDB', 'Express', 'Django', 'PostgreSQL', 'Python', 'Git', 'Github', 'Sass', 'Bootstrap', 'Bulma', 'Heroku', 'Yarn', 'npm']
+
+export const otherTechnologies = ['Illustrator', 'Photoshop', 'Xd', 'Slack', 'Figma', 'Trello']
+
+export const projects = [
+  {
+    key: 'bee',
+    name: 'American Were-Bee in London',
+    description: 'A 7 day project to build a game based on a grid format. Built mostly using classes and array methods in JavaScript and styled with CSS. The game has a number of screens the user can interact with as well as the game play area. In the game there are multiple obstacles to avoid and means by which to score points.',
+    repo: 'https://bit.ly/read-bee-dot-md',
+    deployed: 'https://bit.ly/were-bee',
+    image: bee,
+  },
+  {
+    key: 'weather',
+    name: 'World Weather App',
+    description: 'Based on a hackathon style build, this project looked to quickly pull together a front end application consuming a public API. My partner and I chose a weather theme and pair-coded the app. The front end is built with React, SASS and Bulma.',
+    repo: 'https://bit.ly/rained-me',
+    deployed: 'https://bit.ly/world-weather-app-rk',
+    image: weather,
+  },
+  {
+    key: 'gods',
+    name: 'Trust Pistis',
+    description: 'Full-stack MERN app with CRUD functionality.Based on the TrustPilot website with a theme of Greek Gods. Co-built the backend with some later schema and request functionality added individually. Coded and styled the home and profile pages while giving support and guidance to colleagues with the coding of other components in the app.',
+    repo: 'https://bit.ly/read-melete',
+    deployed: 'https://bit.ly/trust-pistis',
+    image: gods,
+  },
+  {
+    key: 'buhjit',
+    name: 'Buh-jit',
+    description: 'Full-stack application built with Django, PostgreSQL, React and Node.js. A budget management application with a focus on the user. Uses a simple and intuitive interface allowing users to add, edit and delete transactions with a dashboard to display these.',
+    repo: 'https://bit.ly/receipt-me',
+    deployed: 'https://bit.ly/buh-jit',
+    image: buhjit,
+  }
+]
+
+export const experience = [
+  {
+    key: 'ga',
+    company: 'General Assembly',
+    title: 'Software Engineering Immersive',
+    description: [
+      'An intensive full time program to build and solidify software engineering knowledge  foundations.',
+      'The program combines group and individual projects with practical lectures and labs in a fast-paced environment designed to mimic industry.'
+    ],
+    image: ga,
+    date: 'May 2021 - Jul 2021',
+  },
+  {
+    key: 'tfl',
+    company: 'Transport for London',
+    title: 'Product & Industrial Designer',
+    description: [
+      'Designed textiles and interior design components for 6 of London Underground’s Tube Lines covering over 40% of the network.',
+      'Liaised with multifunctional teams to support the design and delivery of two new rolling stock projects - DLR and Deep Tube Upgrade Programme'
+    ],
+    image: tfl,
+    date: 'Apr 2020 - May 2021',
+  },
+  {
+    key: 'eclipse',
+    company: 'Eclipse Blind Systems',
+    title: 'Product Engineer',
+    description: [
+      'Product Expert for SMART and motorised components; product area driving gross revenue growth of +80% YoY.',
+      'Project Manager for new roller hardware. Integrated project requiring collaboration with internal and external stakeholders and the board of directors to justify and manage project budget allocation.',
+      'Senior member of the team responsible for supporting colleagues with design, CAD and legacy product knowledge.'
+    ],
+    image: eclipse,
+    date: 'Jul 2018 - Mar 2020',
+  },
+  {
+    key: 'step',
+    company: 'STEP3D',
+    title: 'Product Design Engineer',
+    description: [
+      'Design Lead and Project Manager responsible for taking projects from concept, to prototyping and manufacture.',
+      'Created mechatronic prototypes and concept CAD for hydrogen vehicles.',
+      'Built effective client relationships across industries ranging from medical devices to educational toys.'
+    ],
+    image: step,
+    date: 'Mar 2017 - Jun 2018',
+  },
+  {
+    key: 'strathclyde',
+    company: 'University of Strathclyde',
+    title: 'MEng Product Design Engineering with Distinction',
+    description: [
+    ],
+    image: strathy,
+    date: 'Sep 2012 - Jun 2017',
+  }
+]
